cedcc3826a8d68277a5d2e34958ded89aa08d585
onimusha-dev <musaddikulislam007@google.com>
2025-11-23 12:56:38 +0530

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './components/theme/index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
